5eaf3a5e0bcaed5555964c8129f2414a457b2131
Hugzy <frede.madsen3@gmail.com>
2018-10-20 11:13:48 +0200

    added json load and overwrite util functions

---
+++ server/jsonRoutine.js
@@ -0,0 +1,14 @@
+const util = require('util');
+const fs = require('fs');
+let readFile = util.promisify(fs.readFile);
+let writeFile = util.promisify(fs.writeFile);
+
+//Overwrites the current persistence.json file with whatever data is if it is valid json
+function overwrite(data, path = './server/persistence.json') {
+    return writeFile(path, JSON.stringify(data));S
+}
+
+//Loads a jsonfile asynchronusly, can optionally specify a new path
+function load(path = './server/persistence.json') {
+    return readFile(path, 'utf8')
+}
