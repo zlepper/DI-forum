50d295d0aefeca2585239982d1a24f0bb2a30e26
Tanya <tanja331@live.dk>
2018-10-20 15:52:04 +0200

    added persistence to thread.

--- server/thread.js
+++ server/thread.js
@@ -1,20 +1,33 @@
 var uuid = require("uuid/v4");
-
-let obj = {};
+let jsonRoutine = require("./jsonRoutine");
 
 exports.addThreadHandler = function(app) {
     var threads = [];
 
-    app.get('/thread', (req, res, next) => res.json(threads));
-
-    app.post('/thread', (req, res, next) => {
-
-        req.body.id = uuid();
-        var threadName = req.body.threadName;
-        console.log(threadName);
-        threads.push(req.body);
-        res.send(`message received ${threadName}`);
+    app.get('/thread', (req, res, next) => {
+        var subforum = req.query.subforum;
+        console.log("subforum: " + subforum)
+        if(!typeof subforum === undefined ){
+            const result = threads.filter(thread => thread.subforum === subforum);
+            console.log("subforum stuff" + result)
+;            res.json(result);
+        } else {
+            console.log("nonsubforum stuff")
+            console.log(threads);
+            res.json(threads);
+        }
     });
 
-}
+    app.post('/thread', async (req, res, next) => {
+
+        const threads = await jsonRoutine.loadJSON('threads.json');
+        req.body.id = uuid();
+        var threadName = req.body.threadName;
+        console.log(`thread name ${threadName}`);
+        threads.push(req.body);
+        await jsonRoutine.overwrite(threads, 'threads.json')
+        res.json(req.body);
+    });
+
+};
 
