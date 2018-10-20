372ba1cbe3a8229a7cc44098178e196d3234a6ac
Tanya <tanja331@live.dk>
2018-10-20 13:48:08 +0200

    added id to thread.

--- server/thread.js
+++ server/thread.js
@@ -1,11 +1,15 @@
+var uuid = require("uuid/v4");
 
 let obj = {};
 
 exports.addThreadHandler = function(app) {
 
+
     app.get('/thread', (req, res, next) => res.send(JSON.stringify(obj)));
 
     app.post('/thread', (req, res, next) => {
+
+        req.body.id = uuidv4();
         var threadName = req.body.threadName;
         console.log(threadName);
         obj = {
