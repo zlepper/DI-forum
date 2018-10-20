8a6410134723a7ec0490f4bfaf1146e659d629bd
sharanka <sharanka96@hotmail.com>
2018-10-20 13:49:37 +0200

    #2 Added uuid's to subforums

--- server/subforum.js
+++ server/subforum.js
@@ -1,10 +1,14 @@
+const uuidv4 = require('uuid');
+
 exports.subforumHandler= function(app) {
     var subforum = [];
 
     app.post('/subforums', (req, res) =>{
+        req.body.id = uuidv4();
         subforum.push(req.body);
         console.log(req.body);
         res.json("stored object: " + req.body.title);
+
     });
 
     app.get('/subforums', (req, res) => {
