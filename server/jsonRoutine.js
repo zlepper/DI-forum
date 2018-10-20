9fff41f14337144bd909eaf6181ba19f4d686ed3
Hugzy <frede.madsen3@gmail.com>
2018-10-20 11:15:56 +0200

    removed wild character

--- server/jsonRoutine.js
+++ server/jsonRoutine.js
@@ -5,7 +5,7 @@ let writeFile = util.promisify(fs.writeFile);
 
 //Overwrites the current persistence.json file with whatever data is if it is valid json
 function overwrite(data, path = './server/persistence.json') {
-    return writeFile(path, JSON.stringify(data));S
+    return writeFile(path, JSON.stringify(data));
 }
 
 //Loads a jsonfile asynchronusly, can optionally specify a new path
